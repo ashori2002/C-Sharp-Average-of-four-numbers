d0f29433dd8c73897e313297e2b999f612ed894b
mohammad ashori <mohammadashori2002@gmail.com>
2022-11-28 23:08:38 +0330

    With this simple program written in C# language in the application console class, you can receive four numbers from the user and then calculate their average for you

---
+++ pr2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double avg=0;
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Enter number "+(i+1)+" :");
+                avg += Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.ForegroundColor = (ConsoleColor)2;
+            Console.WriteLine("AVG ===> " + (avg /= 4)); 
+        }
+    }
+}
